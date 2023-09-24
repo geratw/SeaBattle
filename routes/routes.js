5cbbb812d4679b17f86e52f7a1f87c5500733c34
brenettg <brenettg@student.21-school.ru>
2023-09-25 00:49:59 +0300

    create window link, it remains to add the game

--- routes/routes.js
+++ routes/routes.js
@@ -1,21 +1,28 @@
 const { Router } = require("express");
-const controller = require("../controllers/controller.js");
+const registerController = require("../controllers/register.js");
+const loginController = require("../controllers/login.js");
+const bestPlayerController = require("../controllers/bestPlayers.js");
+const addNewGameController = require("../controllers/addNewGame.js");
+const myStatsController = require("../controllers/myStats.js");
 
 const router = Router();
 
 // GET /register — клиент регистрируется.
-router.post("/api/signUp", controller.register);
+router.get("/signUp", registerController.singUp);
+router.post("/api/signUp", registerController.register);
 
 // GET /login — вход в систему.
-router.get("/api/signIn", controller.login);
+router.get("/signIn", loginController.singIn);
+router.get("/api/signIn", loginController.login);
 
 // // GET /bestPlayers — получить массив объектов игроков, у которых число игр больше 10.
-router.get("/api/bestPlayers", controller.bestPlayers);
+router.get("/bestPlayers", bestPlayerController.bestPlayers);
 
 // // POST / —  добавление игры.
-router.post("/api/", controller.addNewGame);
+router.post("/api/", addNewGameController.addNewGame);
 
 // // get /myStats/:user_id — получение данных о пользователе.
-router.get("/api/myStats/:user_id", controller.myStats);
+router.get("/myStats", myStatsController.myStatsInfo);
+router.get("/api/myStats/:user_id", myStatsController.myStats);
 
 module.exports = router;
